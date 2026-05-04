05fd94f6b27c71c94e5a2fb6eadb18ccc97a4065
MouriNaruto <Kenji.Mouri@outlook.com>
2026-05-05 06:06:55 +0800

    Add Mile.Mobility.Test project.

---
+++ Mile.Mobility.Test/Mile.Mobility.Test.Mobility.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Mouri Internal Library Essentials
+ * FILE:       Mile.Mobility.Test.Mobility.cpp
+ * PURPOSE:    Implementation for Mile.Mobility MobilityOS Test
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Mile.Mobility.Portable.Types.h>
